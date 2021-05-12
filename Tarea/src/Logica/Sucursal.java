5bba557538fc852019f92a3c9fae37c99627f7f5
Nawel-Gasparini <84103030+Nawel-Gasparini@users.noreply.github.com>
2021-05-12 18:58:03 -0300

    Update Sucursal.java
    Setters y constructores

--- Tarea/src/Logica/Sucursal.java
+++ Tarea/src/Logica/Sucursal.java
@@ -9,20 +9,43 @@ public class Sucursal {
     LinkedList<Empleado> listaEmpleados = new LinkedList<Empleado>
     LinkedList<EmpJornalero> listaEmpleadosJornaleros = new LinkedList<EmpJornalero>
     LinkedList<EmpMensual> listaEmpleadosMensuales = new LinkedList<EmpMensual>
-
+    
+    this.nombre = "";
+	this.identificador = 0;
+	this.direccion = "";
+	this.telefono = "";
+	this.rut = 0;
+    
+    
+    
+    public void setnombre() {
+	   this.nombre = nombre;
+    }
     public String getNombre(){
         return nombre;
+    }
+    public void setidentificador() {
+	   this.identificador = identificador;
     }
     public int getIdentificador() {
         return identificador;
     }
-    public String getDireccion(){
+    public void setdireccion() {
+	   this.direccion = direccion;
+    }
+    public String getdireccion(){
         return direccion;
     }
-    public String getTelefono(){
+    public void settelefono() {
+	   this.telefono = telefono;
+    }
+    public String gettelefono(){
         return telefono;
     }
-    public long getRut(){
+    public void setrut() {
+	   this.rut = rut;
+    }
+    public long getrut(){
         return rut;
     }
 }
